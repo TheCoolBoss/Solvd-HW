cb0e96cf66c8f21c2140387c242eb437d5c9233e
TheCoolBoss <nguyenst22@up.edu>
2023-05-02 06:50:05 -0700

    Quick changes

--- Block 1/HW 2/com/hw2/Main.java
+++ Block 1/HW 2/com/hw2/Main.java
@@ -1,26 +1,24 @@
 package com.hw2;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Main 
 {
     public static void main(String[] args)
     {
-        Logger log = Logger.getLogger("Main");
-        FileHandler fh;
+        final Logger LOGGER = LogManager.getLogger(Main.class);
+        LinkedList<Integer> testList = new LinkedList<Integer>(1);
+        System.out.println(LOGGER.isInfoEnabled());
+        testList.addNode(2);
+        testList.addNode(3);
+        testList.printList(testList);
+        testList.removeNode(5);
+        testList.removeNode(3);
+        testList.printList(testList);
 
-        try
-        {
-            fh = new FileHandler("logs.log");
-            log.addHandler(fh);
-        }
-
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input a law firm name");
         String name = scanner.nextLine();
@@ -49,32 +47,14 @@ public class Main
         Helpers.initCases(theLaw);
         Helpers.initClients(theLaw);
 
+        LOGGER.info("test");
         for (LawFirm lawFirm : lawFirms) 
         {
-            log.info(lawFirm.toString());    
+            LOGGER.info(lawFirm.toString());    
         }
         Helpers.printBreak();
         Client client1 = theLaw.getClients().get(0);
 
-        //Used for testing
-        //client1.addCase(theLaw.cases.get(0));
-        //client1.addCase(theLaw.getCases().get(1));
-
-        theLaw.getLawyers().get(0).fire();
-        Case firedCase = new Case("Fired Case", "The Law", "Lawyer", 6);
-
-        //Attempt to add a case to a fired lawyer
-        try
-        {
-            theLaw.getLawyers().get(0).addCase(firedCase);
-        }
-
-        catch (InvalidLicenseException ile)
-        {
-            log.info(ile.getMessage());
-        }
-
-        //Attempt to print costs when a client has no cases
         try
         {
             client1.printCosts(theLaw);
@@ -82,20 +62,23 @@ public class Main
 
         catch (NoCasesFoundException ncfe)
         {
-           log.info(ncfe.getMessage());
+            LOGGER.error(ncfe.getMessage());
+            LOGGER.info("Adding cases from law firm " + theLaw.getName());
+            for (Case c : theLaw.getCases()) 
+            {
+                client1.addCase(c);
+            }
         }
 
-        //Attempt to add an employee to a closed law firm (should fail)
-        theLaw.closeDown();
+        //Need to figure out how to just loop above
         try
         {
-            theLaw.addEmployee(new Lawyer("A", "b", new Plan(5.0), new License("t"), null, "1", 9));
+            client1.printCosts(theLaw);
         }
 
-        catch (ClosedLawFirmException clfe)
+        catch (NoCasesFoundException ncfe)
         {
-            log.info(clfe.getMessage());
+            LOGGER.error(ncfe.getMessage());
         }
-        
     }    
 }
