c74033a7b2597356db73dbd8f70f094adab5f50f
TheCoolBoss <nguyenst22@up.edu>
2023-05-09 10:55:36 -0700

    Hopefully fixes gitignore issues

--- solvd/src/main/java/com/solvd/hw/enums/Session.java
+++ solvd/src/main/java/com/solvd/hw/enums/Session.java
@@ -2,7 +2,13 @@ package com.solvd.hw.enums;
 
 public enum Session 
 {
-    IN_PERSON,
-    REMOTE;
+    IN_PERSON("At office"),
+    REMOTE("Online");
 
+    private String location;
+
+    Session(String loc)
+    {
+        this.location = loc;
+    }
 }
